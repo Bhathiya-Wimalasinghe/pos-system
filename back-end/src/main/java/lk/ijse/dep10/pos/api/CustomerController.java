edc3d78144e8c0a4df6459730751c5253bb30006
Bhathiya Wimalasinghe <bhathiyawimalasinghe@gmail.com>
2023-06-01 08:50:07 +0530

    CustomerController and CustomerDTO was created

--- back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
@@ -1,4 +1,22 @@
 package lk.ijse.dep10.pos.api;
 
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customers")
+@CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    private BasicDataSource pool;
+
+    @GetMapping
+    public void getCustomer(@RequestParam(value = "q", required = false) String query) {
+        System.out.println("getCustomer()");
+    }
+
+
 }
